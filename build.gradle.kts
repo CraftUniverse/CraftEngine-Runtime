88d28613a3a04df50fefd0e06348906b38f3b60f
Turboman3000 <turboman@turboman.de>
2025-02-11 18:40:23 +0100

    build: add Minestom snapshots dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("net.minestom:minestom-snapshots:96cedb1bab")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
