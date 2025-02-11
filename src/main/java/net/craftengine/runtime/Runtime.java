77bd2dfbd175e1d30f1860fb3df9d5f5a57b4c0d
Turboman3000 <turboman@turboman.de>
2025-02-11 18:46:56 +0100

    feat(Runtime): Add server port configuration and start Minecraft server

--- src/main/java/net/craftengine/runtime/Runtime.java
+++ src/main/java/net/craftengine/runtime/Runtime.java
@@ -1,7 +1,24 @@
 package net.craftengine.runtime;
 
+import net.minestom.server.MinecraftServer;
+
 public class Runtime {
+    public static int SERVER_PORT = 25565;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int argIndex = 0;
+
+        // Parse command line arguments
+        for(var arg : args) {
+            if(arg.equalsIgnoreCase("--port") || arg.equalsIgnoreCase("-p")) {
+                SERVER_PORT = Integer.parseInt(args[argIndex + 1]);
+            }
+
+            argIndex++;
+        }
+
+        MinecraftServer server = MinecraftServer.init();
+
+        server.start("0.0.0.0", SERVER_PORT);
     }
 }
