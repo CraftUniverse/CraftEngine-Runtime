79a749f928b486cbfe2fb071fc5db3597476cbf9
Turboman3000 <turboman@turboman.de>
2025-02-11 18:56:55 +0100

    build: update Logback to version 1.5.16 and add NopStatusListener

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ dependencies {
     implementation("net.minestom:minestom-snapshots:96cedb1bab")
 
     // Logback
-    implementation("ch.qos.logback:logback-core:1.5.8")
-    implementation("ch.qos.logback:logback-classic:1.5.8")
+    implementation("ch.qos.logback:logback-core:1.5.16")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
