b6b2841a3207713074c8cf3ebb6aab429618215f
Turboman3000 <turboman@turboman.de>
2025-02-11 19:24:04 +0100

    feat(Runtime): Add logic port, offline mode, and MojangAuth integration

--- src/main/java/net/craftengine/runtime/Runtime.java
+++ src/main/java/net/craftengine/runtime/Runtime.java
@@ -1,24 +1,46 @@
 package net.craftengine.runtime;
 
+import net.craftengine.runtime.debug.Git;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.extras.MojangAuth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Runtime {
-    public static int SERVER_PORT = 25565;
+    public static int MINECRAFT_PORT = 25565;
+    public static int LOGIC_PORT = 48528;
+    public static boolean OFFLINE_MODE = false;
+
+    private static Logger logger = LoggerFactory.getLogger(Runtime.class);
 
     public static void main(String[] args) {
         int argIndex = 0;
 
         // Parse command line arguments
-        for(var arg : args) {
-            if(arg.equalsIgnoreCase("--port") || arg.equalsIgnoreCase("-p")) {
-                SERVER_PORT = Integer.parseInt(args[argIndex + 1]);
+        for (var arg : args) {
+            if (arg.equalsIgnoreCase("--minecraft_port") || arg.equalsIgnoreCase("-mp")) {
+                MINECRAFT_PORT = Integer.parseInt(args[argIndex + 1]);
+            } else if (arg.equalsIgnoreCase("--logic_port") || arg.equalsIgnoreCase("-lp")) {
+                LOGIC_PORT = Integer.parseInt(args[argIndex + 1]);
+            } else if (arg.equalsIgnoreCase("--offline") || arg.equalsIgnoreCase("-o")) {
+                OFFLINE_MODE = true;
             }
 
             argIndex++;
         }
 
+        logger.info("Server Port: {}", MINECRAFT_PORT);
+        logger.info("Runtime commit \"{}\" on branch \"{}\"", Git.commit(), Git.branch());
+
         MinecraftServer server = MinecraftServer.init();
 
-        server.start("0.0.0.0", SERVER_PORT);
+        // Check if the application is not in offline mode
+        if (!OFFLINE_MODE) {
+            MojangAuth.init();
+        } else {
+            logger.warn("Server is Running in offline mode!");
+        }
+
+        server.start("0.0.0.0", MINECRAFT_PORT);
     }
 }
