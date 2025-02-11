3222ffbcc6dde41cfbd34d9d94db556a36344df9
Turboman3000 <turboman@turboman.de>
2025-02-11 18:45:54 +0100

    feat: Add Logback logging with custom highlight converter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "net.craftengine"
@@ -11,6 +12,10 @@ repositories {
 
 dependencies {
     implementation("net.minestom:minestom-snapshots:96cedb1bab")
+
+    // Logback
+    implementation("ch.qos.logback:logback-core:1.5.8")
+    implementation("ch.qos.logback:logback-classic:1.5.8")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
