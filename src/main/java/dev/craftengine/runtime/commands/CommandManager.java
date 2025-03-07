03ea5c7315cbd63cda9ec0eecd13710610c9d3c8
Turboman3000 <turboman@turboman.de>
2025-03-07 01:52:32 +0100

    fix: add unknown command callback for command manager

--- src/main/java/dev/craftengine/runtime/commands/CommandManager.java
+++ src/main/java/dev/craftengine/runtime/commands/CommandManager.java
@@ -3,10 +3,16 @@ package dev.craftengine.runtime.commands;
 import dev.craftengine.runtime.Runtime;
 import dev.craftengine.runtime.commands.buildin.HelpCommand;
 import dev.craftengine.runtime.commands.buildin.LevelCommand;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class CommandManager {
+    private final MiniMessage mm = MiniMessage.miniMessage();
 
     public CommandManager(net.minestom.server.command.CommandManager commandManager) {
+        commandManager.setUnknownCommandCallback((sender, command) -> {
+            sender.sendMessage(mm.deserialize("<red>Unknown command: /" + command));
+        });
+
         if (Runtime.DEVELOPER_MODE) {
             commandManager.register(new LevelCommand());
             commandManager.register(new HelpCommand());
--- src/main/java/dev/craftengine/runtime/commands/buildin/HelpCommand.java
+++ src/main/java/dev/craftengine/runtime/commands/buildin/HelpCommand.java
@@ -1,13 +1,16 @@
 package dev.craftengine.runtime.commands.buildin;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.command.builder.Command;
 
 public class HelpCommand extends Command {
+    private final MiniMessage mm = MiniMessage.miniMessage();
+
     public HelpCommand() {
         super("help");
 
-        setDefaultExecutor(((sender, context) -> {
-
-        }));
+        setDefaultExecutor((sender, context) -> {
+            sender.sendMessage(mm.deserialize("<red>Not implemented yet!"));
+        });
     }
 }
